6de2571dd8058f21b4842c834920a553d466d3ac
kuromika <kuromika.dev@gmail.com>
2023-06-08 15:53:02 -0500

    Toggle blog details - 5.6, 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,25 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsOpen((prev) => !prev);
+  }
+
+  return (
+    <div className="blog">
+      <p>{blog.title} <button type='button' onClick={toggleVisibility}>{isOpen ? 'hide' : 'show'}</button></p>
+      {isOpen &&
+        <div>
+          <p>{blog.url}</p>
+          <p>{blog.likes}<button>like</button></p>
+          <p>{blog.author}</p>
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
