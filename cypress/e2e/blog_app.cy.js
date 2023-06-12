2d4fc68373e887ac082007ca4e9c531f797c94f2
kuromika <kuromika.dev@gmail.com>
2023-06-12 15:13:01 -0500

    Test login - 5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -3,9 +3,32 @@
 describe('Blog app', () => {
   beforeEach(() => {
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const user = {
+      username: 'kuromika',
+      password: 'password',
+      name: 'mikaela'
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
     cy.visit('')
   })
   it('Login form is shown', () => {
     cy.get('button').contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('input#username').type('kuromika')
+      cy.get('input#password').type('password')
+      cy.get('button').click()
+      cy.get('p.success')
+      cy.contains('mikaela logged in')
+    })
+    it('fails with incorrect credentials', function () {
+      cy.get('input#username').type('salieri')
+      cy.get('input#password').type('badpassword')
+      cy.get('button').click()
+      cy.should('not.contain', 'salieri logged in')
+      cy.get('p').should('not.have.class', 'success').and('have.class', 'error')
+    })
   })
 })
